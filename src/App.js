a403ea1efba06319df2138291ca2f88b3962ffba
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-06 09:26:11 +0100

    nav splited

--- src/App.js
+++ src/App.js
@@ -6,26 +6,42 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 function NavBar() {
-  const [query, setQuery] = useState("");
   return (
     <nav className='nav-bar'>
-      <div className='logo'>
-        <span role='img'>🍿🍿</span>
-        <h1>usePopcorn</h1>
-      </div>
-      <input
-        className='search'
-        type='text'
-        placeholder='Search movies...'
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <p className='num-results'>
-        Found <strong>X</strong> results
-      </p>
+      <Logo />
+      <Search />
+      <NumResult />
     </nav>
   );
 }
+function Logo() {
+  return (
+    <div className='logo'>
+      <span role='img'>🍿🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
+function Search() {
+  const [query, setQuery] = useState("");
+  return (
+    <input
+      className='search'
+      type='text'
+      placeholder='Search movies...'
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+function NumResult() {
+  return (
+    <p className='num-results'>
+      Found <strong>X</strong> results
+    </p>
+  );
+}
+
 function Main() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
