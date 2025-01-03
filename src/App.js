332a2da50395fda2d6329dd08faf0849b418407e
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-01-03 23:41:38 +0100

    prep

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import Nav from "./Nav";
+
 export default function App() {
-  return <div>popcorn</div>;
+  return (
+    <>
+      <Nav />
+    </>
+  );
 }
---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul className='nav'>
+        <li>Home</li>
+        <li>History</li>
+        <li>Customers</li>
+        <li>Services</li>
+        <li>Contact us</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
